881d177be38d221e0f8bdcf0b0f91082b6072b08
Manikandan Swaminathan <mani.bk31@gmail.com>
2016-11-29 22:35:05 +0000

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Diet Share</title>
 
         <link rel="stylesheet" type="text/css" href="{{asset('/dist/semantic.min.css')}}">
 <script src="{{asset('/dist/semantic.min.js')}}"></script>
